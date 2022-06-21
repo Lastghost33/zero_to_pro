9f1b0b96b3cbb5df76f142c56843ac65ad60b91f
Lastghost33 <tnhn33dyg@gmail.com>
2022-06-21 12:17:26 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/zero_to_pro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zero_to_pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/101/text_learn_view.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class TextLearnView extends StatelessWidget {
+  const TextLearnView({Key? key, this.userName}) : super(key: key);
+  final String name = 'Lastghost';
+
+  final String? userName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              (' $name Everbody Wants to me my enemy'),
+              style: const TextStyle(
+                color: Colors.red,
+                decoration: TextDecoration.lineThrough,
+                fontSize: 24,
+                letterSpacing: 2,
+              ),
+            ),
+            Text(
+              ('Everbody Wants to me my enemy' * 10),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline3
+                  ?.copyWith(color: Colors.black),
+            ),
+            Text(
+              ('${userName ?? ''} Everbody Wants to me my enemy' * 10),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: ProcejtStyle.imagineDragons,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProcejtStyle {
+  static TextStyle imagineDragons = const TextStyle(
+    color: Colors.yellow,
+    decoration: TextDecoration.lineThrough,
+    fontSize: 24,
+    letterSpacing: 2,
+  );
+}
